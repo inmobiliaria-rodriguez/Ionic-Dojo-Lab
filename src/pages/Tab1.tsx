ff739d581cdb1b3057e6838624e232c4e5d7ce55
sebastian-ochoa1 <sebastian.ochoa1@udea.edu.co>
2023-11-10 19:29:06 -0500

    Se sube Dojo funcional

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,16 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Tab1.css';
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonViewDidEnter,
+  IonList,
+} from "@ionic/react";
+import "./Tab1.css";
+import { ItemCard } from "../components/ItemCard";
+import { Item } from "../models/item.model";
 
 const Tab1: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const getItem = async (url: string) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const item: Item = {
+      name: data.name,
+      image: data.sprites.default,
+    };
+    return item;
+  };
+  const getItems = async () => {
+    let items: Item[] = [];
+    for (let i = 1; i <= 16; i++) {
+      const item = await getItem(`https://pokeapi.co/api/v2/item/${i}`);
+      items.push(item);
+    }
+    return items;
+  };
+
+  useIonViewDidEnter(() => {
+    getItems().then((items) => {
+      setItems(items);
+    });
+  });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Poké-balls list</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-
+        <IonList>
+          {items.map((item, index) => (
+            <ItemCard key={index} item={item} />
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,17 +1,75 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Tab2.css';
-
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonButton,
+  IonCardSubtitle,
+} from "@ionic/react";
+import "./Tab2.css";
+import { Pokemon } from "../models/pokemon.model";
 const Tab2: React.FC = () => {
+  const [pokemon, setPokemon] = useState<Pokemon>({
+    name: "",
+    image: "",
+    types: [],
+  });
+  const getPokemon = async (url: string) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const pokemon: Pokemon = {
+      name: data.name,
+      image: data.sprites.front_default,
+      types: data.types.map((type: any) => type.type.name),
+    };
+    return pokemon;
+  };
+  const getRandomPokemon = async () => {
+    const id = Math.floor(Math.random() * 1291) + 1;
+    const pokemon = await getPokemon(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    return pokemon;
+  };
+
   return (
-    <IonPage >
+    <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Select random pokemon</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen >
+      <div className="container">
+        {pokemon.image !== "" ? (
+          <IonCard className="icon-card">
+            <img src={pokemon.image} alt="Pokemon image" />
+            <IonCardHeader>
+              <IonCardTitle>{pokemon.name}</IonCardTitle>
+              <IonCardSubtitle>
+                Pokemon type(s):
+                {pokemon.types.join(", ")}
+              </IonCardSubtitle>
+            </IonCardHeader>
+          </IonCard>
+        ) : (
+          <IonCard className="icon-card">
+            <IonCardHeader>
+              <IonCardTitle>Select one pokemon</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
+        )}
 
-      </IonContent>
+        <IonButton
+          onClick={() =>
+            getRandomPokemon().then((pokemon) => setPokemon(pokemon))
+          }
+        >
+          Pick one
+        </IonButton>
+      </div>
     </IonPage>
   );
 };
